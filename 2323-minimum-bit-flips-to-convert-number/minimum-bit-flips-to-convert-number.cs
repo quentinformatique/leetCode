a2f1aae687a27318bfdca8a37879170bf85fdfa3
quentinformatique <80545291+quentinformatique@users.noreply.github.com>
2024-09-11 23:59:05 +0200

    Time: 22 ms (40.43%) | Memory: 27.1 MB (15.96%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int MinBitFlips(int start, int goal) {
+        int xor = start ^  goal;
+        int count = 0;
+        while (xor != 0){
+            xor = xor & (xor - 1);
+            count++;
+        }
+        return count;
+    }
+}
