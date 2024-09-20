5eafcf45a79cb392ddd65fb9e47f33b59482a110
quentinformatique <80545291+quentinformatique@users.noreply.github.com>
2024-09-20 10:13:38 +0200

    Time: 34 ms (44.69%) | Memory: 45.3 MB (20.39%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,15 +1,15 @@
 class Solution {
   public String shortestPalindrome(String s) {
-    final String t = new StringBuilder(s).reverse().toString();
-    for (int i = 0; i < t.length(); i++) {
+    final String reverseS = new StringBuilder(s).reverse().toString();
+    for (int i = 0; i < reverseS.length(); i++) {
       // Check if the original string 's' starts with the current suffix of 't'
-      if (s.startsWith(t.substring(i))) {
+      if (s.startsWith(reverseS.substring(i))) {
         // If a matching prefix-suffix is found, return the result by adding
         // the unmatched part of 't' to the front of 's' to form the palindrome
-        return t.substring(0, i) + s;
+        return reverseS.substring(0, i) + s;
       }
     }
     // if no sub - palidromes found, concat s and t
-    return t + s;
+    return reverseS + s;
   }
 }
